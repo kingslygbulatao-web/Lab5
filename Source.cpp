c812cc8b4849abc9ae09729a9c51ca22bf008502
Me <planetaryaxis93@gmail.com>
2026-02-12 17:36:52 -0800

    Copy paste Initial code

--- Source.cpp
+++ Source.cpp
@@ -2,8 +2,25 @@
 #include <iostream>
 using namespace std;
 
+#include <iostream>
+using namespace std;
+
+int swap(int &, int &);
+
 int main() {
-    cout << "Hello, World!" << endl;
+    int x = 5, y = 10;
+    cout << "x = " << x << "  | y = " << y << endl;
+
+    cout << "Swapping...\n";
+    int sum = swap(x, y);
+    cout << "x = " << x << " | y = " << y << endl;
+    cout << "sum = " << sum << endl;
     return 0;
-    
+}
+
+int swap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+    return a + b;
 }
